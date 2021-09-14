601c46c5deaf4f7825a2c38bfabebc26aa6bf71e
gwt805 <1973735972@qq.com>
2021-09-14 22:44:52 +0800

    finish

--- jisuanqi.py
+++ jisuanqi.py
@@ -6,23 +6,27 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtGui import QIcon
 
-class Jisuanqi(QtWidgets.QTabWidget):
+class Jisuanqi(QtWidgets.QWidget):
     def __init__(self):
         super(Jisuanqi,self).__init__()
         self.setupUi(self)
         self.retranslateUi(self)
     def setupUi(self, jisuanqi):
         jisuanqi.setObjectName("jisuanqi")
-        jisuanqi.resize(358, 342)
+        jisuanqi.resize(358, 372)
+        # fix window size
         jisuanqi.setFixedWidth(300)
         jisuanqi.setFixedHeight(300)
+        jisuanqi.setWindowIcon(QIcon('img/jsq.png'))
         # let the window center
         screen = QDesktopWidget().screenGeometry()
         jsq_size = self.geometry()
         jisuanqi.move(int((screen.width() - jsq_size.width()) / 2) , int((screen.height() - jsq_size.height()) / 2))
+
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -32,13 +36,9 @@ class Jisuanqi(QtWidgets.QTabWidget):
         self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout_2 = QtWidgets.QGridLayout()
         self.gridLayout_2.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
-        self.gridLayout_2.setSpacing(6)
+        self.gridLayout_2.setHorizontalSpacing(3)
+        self.gridLayout_2.setVerticalSpacing(6)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.lineEdit = QtWidgets.QLineEdit(jisuanqi)
-        self.lineEdit.setStyleSheet("")
-        self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit.grabKeyboard() 
-        self.gridLayout_2.addWidget(self.lineEdit, 1, 0, 1, 4)
         self.pushButton_4 = QtWidgets.QPushButton(jisuanqi)
         self.pushButton_4.setObjectName("pushButton_4")
         self.gridLayout_2.addWidget(self.pushButton_4, 3, 1, 1, 1)
@@ -81,80 +81,73 @@ class Jisuanqi(QtWidgets.QTabWidget):
         self.pushButton_14 = QtWidgets.QPushButton(jisuanqi)
         self.pushButton_14.setObjectName("pushButton_14")
         self.gridLayout_2.addWidget(self.pushButton_14, 5, 1, 1, 1)
-        self.pushButton_15 = QtWidgets.QPushButton(jisuanqi)
-        self.pushButton_15.setObjectName("pushButton_15")
-        self.gridLayout_2.addWidget(self.pushButton_15, 5, 2, 1, 1)
-        self.pushButton_16 = QtWidgets.QPushButton(jisuanqi)
-        self.pushButton_16.setObjectName("pushButton_16")
-        self.gridLayout_2.addWidget(self.pushButton_16, 5, 3, 1, 1)
+        self.pushButton_17 = QtWidgets.QPushButton(jisuanqi)
+        self.pushButton_17.setObjectName("pushButton_17")
+        self.gridLayout_2.addWidget(self.pushButton_17, 2, 4, 1, 1)
         self.textEdit = QtWidgets.QTextEdit(jisuanqi)
         self.textEdit.setEnabled(False)
         self.textEdit.setObjectName("textEdit")
-        self.gridLayout_2.addWidget(self.textEdit, 0, 0, 1, 4)
+        self.gridLayout_2.addWidget(self.textEdit, 0, 0, 1, 5)
+        self.lineEdit = QtWidgets.QLineEdit(jisuanqi)
+        self.lineEdit.setStyleSheet("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setFocus()
+        self.gridLayout_2.addWidget(self.lineEdit, 1, 0, 1, 5)
+        self.pushButton_18 = QtWidgets.QPushButton(jisuanqi)
+        self.pushButton_18.setObjectName("pushButton_18")
+        self.gridLayout_2.addWidget(self.pushButton_18, 3, 4, 1, 1)
+        self.pushButton_19 = QtWidgets.QPushButton(jisuanqi)
+        self.pushButton_19.setObjectName("pushButton_19")
+        self.gridLayout_2.addWidget(self.pushButton_19, 4, 4, 1, 1)
+        self.pushButton_16 = QtWidgets.QPushButton(jisuanqi)
+        self.pushButton_16.setObjectName("pushButton_16")
+        self.gridLayout_2.addWidget(self.pushButton_16, 5, 4, 1, 1)
+        self.pushButton_15 = QtWidgets.QPushButton(jisuanqi)
+        self.pushButton_15.setObjectName("pushButton_15")
+        self.gridLayout_2.addWidget(self.pushButton_15, 5, 2, 1, 2)
         self.verticalLayout.addLayout(self.gridLayout_2)
-        # mycode
-        self.ji = '' # 多行文本框赋值用
 
         self.retranslateUi(jisuanqi)
         QtCore.QMetaObject.connectSlotsByName(jisuanqi)
+        # mycode
+        self.ji = '' # 多行文本框赋值用
+        self.pushButton_13.clicked.connect(lambda:self.numss('0'))
+        self.pushButton.clicked.connect(lambda:self.numss('1'))
+        self.pushButton_7.clicked.connect(lambda:self.numss('2'))
+        self.pushButton_8.clicked.connect(lambda:self.numss('3'))
+        self.pushButton_3.clicked.connect(lambda:self.numss('4'))
+        self.pushButton_4.clicked.connect(lambda:self.numss('5'))
+        self.pushButton_5.clicked.connect(lambda:self.numss('6'))
+        self.pushButton_2.clicked.connect(lambda:self.numss('7'))
+        self.pushButton_10.clicked.connect(lambda:self.numss('8'))
+        self.pushButton_11.clicked.connect(lambda:self.numss('9'))
+        self.pushButton_14.clicked.connect(lambda:self.numss('.'))
+        self.pushButton_6.clicked.connect(lambda:self.numss('+'))
+        self.pushButton_9.clicked.connect(lambda:self.numss('-'))
+        self.pushButton_12.clicked.connect(lambda:self.numss('*'))
+        self.pushButton_16.clicked.connect(lambda:self.numss('/'))
+        self.pushButton_17.clicked.connect(lambda:self.numss('('))
+        self.pushButton_18.clicked.connect(lambda:self.numss(')'))
+        self.pushButton_19.clicked.connect(lambda:self.numss('C'))
+        self.pushButton_19.setShortcut('escape')
+        self.pushButton_15.clicked.connect(lambda:self.numss('='))
+        self.lineEdit.returnPressed.connect(lambda:self.numss('='))
 
-        self.pushButton_13.clicked.connect(self.num0)
-        self.pushButton.clicked.connect(self.num1)
-        self.pushButton_7.clicked.connect(self.num2)
-        self.pushButton_8.clicked.connect(self.num3)
-        self.pushButton_3.clicked.connect(self.num4)
-        self.pushButton_4.clicked.connect(self.num5)
-        self.pushButton_5.clicked.connect(self.num6)
-        self.pushButton_2.clicked.connect(self.num7)
-        self.pushButton_10.clicked.connect(self.num8)
-        self.pushButton_11.clicked.connect(self.num9)
-        self.pushButton_14.clicked.connect(self.numdian)
-        self.pushButton_6.clicked.connect(self.numjia)
-        self.pushButton_9.clicked.connect(self.numjian)
-        self.pushButton_12.clicked.connect(self.numcheng)
-        self.pushButton_16.clicked.connect(self.numchu)
-        self.pushButton_15.clicked.connect(self.numdengyu)
-        self.lineEdit.returnPressed.connect(self.numdengyu)
-
-    def num0(self):
-        self.lineEdit.setText(self.lineEdit.text() + '0')
-    def num1(self):
-        self.lineEdit.setText(self.lineEdit.text() + '1')
-    def num2(self):
-        self.lineEdit.setText(self.lineEdit.text() + '2')
-    def num3(self):
-        self.lineEdit.setText(self.lineEdit.text() + '3')
-    def num4(self):
-        self.lineEdit.setText(self.lineEdit.text() + '4')
-    def num5(self):
-        self.lineEdit.setText(self.lineEdit.text() + '5')
-    def num6(self):
-        self.lineEdit.setText(self.lineEdit.text() + '6')
-    def num7(self):
-        self.lineEdit.setText(self.lineEdit.text() + '7')
-    def num8(self):
-        self.lineEdit.setText(self.lineEdit.text() + '8')
-    def num9(self):
-        self.lineEdit.setText(self.lineEdit.text() + '9')
-    def numcheng(self):
-        self.lineEdit.setText(self.lineEdit.text() + '×')
-    def numchu(self):
-        self.lineEdit.setText(self.lineEdit.text() + '/')
-    def numdian(self):
-        self.lineEdit.setText(self.lineEdit.text() + '.')
-    def numjia(self):
-        self.lineEdit.setText(self.lineEdit.text() + '+')
-    def numjian(self):
-        self.lineEdit.setText(self.lineEdit.text() + '-')
-    def numdengyu(self):
-        try:
-            tt = self.lineEdit.text()
-            num = eval(tt.replace('×','*'))
-            self.lineEdit.setText(str(num))
-            self.ji += tt + '=' +str(num) + '\n'
-            self.textEdit.setText(self.ji)
-        except:
-            self.lineEdit.setText('输入有误，请重新输入')
+    def numss(self,nnum):
+        if nnum == 'C':
+            self.lineEdit.clear()
+            self.textEdit.clear()
+        elif nnum == '=':
+            try:
+                tt = self.lineEdit.text()
+                num = eval(tt.replace('×','*'))
+                self.lineEdit.setText(str(num))
+                self.ji += tt + '=' +str(num) + '\n'
+                self.textEdit.setText(self.ji)
+            except:
+                self.lineEdit.setText('输入有误，请重新输入')
+        else:
+            self.lineEdit.setText(self.lineEdit.text() + nnum)
 
     def retranslateUi(self, jisuanqi):
         _translate = QtCore.QCoreApplication.translate
@@ -173,15 +166,19 @@ class Jisuanqi(QtWidgets.QTabWidget):
         self.pushButton_11.setText(_translate("jisuanqi", "9"))
         self.pushButton_13.setText(_translate("jisuanqi", "0"))
         self.pushButton_14.setText(_translate("jisuanqi", "."))
-        self.pushButton_15.setText(_translate("jisuanqi", "="))
+        self.pushButton_17.setText(_translate("jisuanqi", "("))
+        self.pushButton_18.setText(_translate("jisuanqi", ")"))
+        self.pushButton_19.setText(_translate("jisuanqi", "C"))
         self.pushButton_16.setText(_translate("jisuanqi", "/"))
+        self.pushButton_15.setText(_translate("jisuanqi", "="))
+
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Mainwindow = QtWidgets.QWidget()
+    jisuanqi = QtWidgets.QWidget()
     ui = Jisuanqi()
-    ui.setupUi(Mainwindow)
-    Mainwindow.show()
+    ui.setupUi(jisuanqi)
+    jisuanqi.show()
     sys.exit(app.exec_())
 
